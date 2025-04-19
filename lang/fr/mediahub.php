9f8eb29c43c10b1c74b9e4f3eed377fa1eb7e4a0
generation-free <contact@generation-free.biz>
2025-04-19 16:51:50 +0200

    Added translation using Weblate (French)

---
+++ lang/fr/mediahub.php
@@ -0,0 +1,40 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     Howard Lau <howardlau1999@hotmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'born' => '',
+    'collection' => '',
+    'collections' => '',
+    'companies' => '',
+    'disclaimer' => '',
+    'episodes' => '',
+    'first-seen' => '',
+    'genres' => '',
+    'includes' => '',
+    'latest-project' => '',
+    'networks' => '',
+    'no-data' => '',
+    'movie' => '',
+    'movies' => '',
+    'movie-credits' => '',
+    'persons' => '',
+    'plot' => '',
+    'release-date' => '',
+    'seasons' => '',
+    'select-hub' => '',
+    'show' => '',
+    'shows' => '',
+    'title' => '',
+    'tv-credits' => '',
+    'view-collection' => '',
+    'wiki-read' => '',
+];
