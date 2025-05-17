cb2b5feb71cbaece201a36c4cc0d60d8d79ff862
ClassicUA <steelpadss@gmail.com>
2025-05-17 11:04:30 +0200

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: UNIT3D/vendor-cookie-consent
    Translate-URL: https://hosted.weblate.org/projects/unit3d/vendor-cookie-consent/uk/

--- lang/vendor/cookie-consent/uk/texts.php
+++ lang/vendor/cookie-consent/uk/texts.php
@@ -11,6 +11,6 @@ return [
  * @author     HDVinnie <hdinnovations@protonmail.com>
  * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
  */
-    'message' => '',
-    'agree' => '',
+    'message' => 'Ваш досвід роботи на цьому сайті буде покращено, якщо ви дозволите використовувати файли cookie.',
+    'agree' => 'Дозволити файли cookie',
 ];
