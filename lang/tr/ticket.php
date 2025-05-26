3333bd3ab47291b0d85e4c3bcb15aa951e645ca6
Kivanc PAPUR <mkpapur@gmail.com>
2025-05-26 08:32:01 +0200

    Added translation using Weblate (Turkish)

---
+++ lang/tr/ticket.php
@@ -0,0 +1,51 @@
+<?php
+return [
+    /**
+ * NOTICE OF LICENSE.
+ *
+ * UNIT3D Community Edition is open-sourced software licensed under the GNU Affero General Public License v3.0
+ * The details is bundled with this project in the file LICENSE.txt.
+ *
+ * @project    UNIT3D Community Edition
+ *
+ * @author     HDVinnie <hdinnovations@protonmail.com>
+ * @license    https://www.gnu.org/licenses/agpl-3.0.en.html/ GNU Affero General Public License v3.0
+ */
+    'assign' => '',
+    'assigned-staff' => '',
+    'assigned-success' => '',
+    'attachment-limit' => '',
+    'attachments' => '',
+    'attachments-save' => '',
+    'body' => '',
+    'body-enter' => '',
+    'category' => '',
+    'close' => '',
+    'closed' => '',
+    'closed-success' => '',
+    'create-ticket' => '',
+    'created' => '',
+    'created-success' => '',
+    'delete' => '',
+    'deleted-success' => '',
+    'description' => '',
+    'download' => '',
+    'fix-errors' => '',
+    'helpdesk' => '',
+    'no-attach' => '',
+    'note-create-success' => '',
+    'note-destroy-success' => '',
+    'opened-by' => '',
+    'priority' => '',
+    'reopen' => '',
+    'reopen-success' => '',
+    'reset' => '',
+    'staff-notes' => '',
+    'subject' => '',
+    'subject-enter' => '',
+    'submit-ticket' => '',
+    'ticket' => '',
+    'unassign' => '',
+    'unassigned-success' => '',
+    'updated-success' => '',
+];
