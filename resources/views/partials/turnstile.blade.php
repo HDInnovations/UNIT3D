d828e22478328490ba98d1a0b8dd96094190d749
AnabolicsAnonymous <github@unhittable.pw>
2025-06-02 04:14:49 -0500

    add: turnstile blade partial

---
+++ resources/views/partials/turnstile.blade.php
@@ -0,0 +1,7 @@
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer nonce="{{ HDVinnie\SecureHeaders\SecureHeaders::nonce() }}"></script>
+<div class="cf-turnstile" 
+    data-sitekey="{{ config('captcha.turnstile.site_key') }}"
+    data-theme="dark"
+    data-action="{{ Route::is('login') ? 'login' : (Route::is('register') ? 'register' : (Route::is('password.email') ? 'reset' : (Route::is('verification.send') ? 'verify' : 'auth'))) }}"
+    data-size="normal"
+    data-appearance="interactive"></div> 
