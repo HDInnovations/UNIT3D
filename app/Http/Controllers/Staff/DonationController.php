b5894538330cdde7a39513ff3135d70ddccb088a
Oha-you <82098328+Oha-you@users.noreply.github.com>
2026-02-19 00:55:12 +0300

    add: Support multiple donations by the same user, adjust start/end times

--- app/Http/Controllers/Staff/DonationController.php
+++ app/Http/Controllers/Staff/DonationController.php
@@ -73,18 +73,30 @@ class DonationController extends Controller
 
         $donation = Donation::query()->with(['user', 'package'])->findOrFail($id);
         $donation->status = ModerationStatus::APPROVED;
+
+        $latestDonation = Donation::query()
+            ->where('status', '=', ModerationStatus::APPROVED)
+            ->where('user_id', '=', $donation->user->id)
+            ->latest('updated_at')
+            ->first();
+
+        $isLifetime = $donation->package->donor_value === null ||
+            ($latestDonation && $latestDonation->ends_at === null);
+
         $donation->starts_at = $now;
 
-        if ($donation->package->donor_value > 0) {
-            $donation->ends_at = $now->addDays($donation->package->donor_value);
-        } else {
-            $donation->ends_at = null;
+        if ($latestDonation && !$isLifetime) {
+            $donation->starts_at = $latestDonation->ends_at;
+        }
+
+        if (!$isLifetime) {
+            $donation->ends_at = $donation->starts_at->copy()->addDays($donation->package->donor_value);
         }
 
         $donation->user->invites += $donation->package->invite_value ?? 0;
         $donation->user->uploaded += $donation->package->upload_value ?? 0;
         $donation->user->is_donor = true;
-        $donation->user->is_lifetime = $donation->package->donor_value === null;
+        $donation->user->is_lifetime = $isLifetime;
         $donation->user->seedbonus += $donation->package->bonus_value ?? 0;
         $donation->user->save();
 
