56ccbb0b837b121fd4f694fc70030e202bad84c2
Oha-you <82098328+Oha-you@users.noreply.github.com>
2026-02-20 14:22:42 +0300

    add: Select all text when focusing donation gateway inputs

--- resources/views/donation/index.blade.php
+++ resources/views/donation/index.blade.php
@@ -132,8 +132,9 @@
                                 <input
                                     class="form__text"
                                     type="text"
-                                    disabled
+                                    readonly
                                     value="{{ $gateway->address }}"
+                                    x-on:focus="$el.select()"
                                     id="{{ 'gateway-' . $gateway->id }}"
                                 />
                                 <label
@@ -159,8 +160,9 @@
                             <input
                                 class="form__text"
                                 type="text"
-                                disabled
+                                readonly
                                 value="{{ $package->cost }}"
+                                x-on:focus="$el.select()"
                                 id="package-cost"
                             />
                             <label for="package-cost" class="form__label form__label--floating">
@@ -171,6 +173,7 @@
                             <input
                                 class="form__text"
                                 type="text"
+                                autofocus
                                 value=""
                                 id="proof"
                                 name="transaction"
