23b78d956d148dace21cf674c53784483e6188bc
whyujjwal <138800022+whyujjwal@users.noreply.github.com>
2026-05-26 08:09:17 +0000

    fix: set category_id to null on category deletion

--- app/Http/Controllers/Staff/CategoryController.php
+++ app/Http/Controllers/Staff/CategoryController.php
@@ -124,6 +124,9 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category): \Illuminate\Http\RedirectResponse
     {
+        // Set the category_id to null for all requests that have this category_id
+        $category->requests()->update(['category_id' => null]);
+
         $category->delete();
 
         return to_route('staff.categories.index')
