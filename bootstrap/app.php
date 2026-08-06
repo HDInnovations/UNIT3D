3f6edeaf1f4f27ef87e53af0f056e362db94b2f8
Shift <shift@laravelshift.com>
2026-08-06 17:38:58 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->throttleWithRedis();
+
+        $middleware->append([
+            \Illuminate\Foundation\Http\Middleware\InvokeDeferredCallbacks::class,
+            \App\Http\Middleware\BlockIpAddress::class,
+        ]);
+
+        $middleware->replace(\Illuminate\Http\Middleware\TrustProxies::class, \App\Http\Middleware\TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
